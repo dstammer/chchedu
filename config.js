e37be39bca1954b15b2b7c66a41b446b97355c43
unknown <dennis.stammer72@hotmail.com>
2016-01-02 23:53:40 +0100

    latest

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ module.exports.devPort = process.env.PORT || 9010;
 module.exports.prodPort = process.env.PORT || 80;
 
 //module.exports.dbConnection = mongoose.createConnection("mongodb://localhost:27017/chchedu");
-module.exports.dbConnection = mongoose.createConnection("mongodb://<dbuser>:<dbpassword>@ds037165.mongolab.com:37165/heroku_x0h82d13");
+module.exports.dbConnection = mongoose.createConnection("mongodb://dennis:dennis@ds037165.mongolab.com:37165/heroku_x0h82d13");
